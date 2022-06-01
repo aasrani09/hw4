1c971389d5c8ee0b73c7a65f8238459af3e6722e
aasrani09 <aarti.asrani@kellogg.northwestern.edu>
2022-06-01 02:36:54 +0000

    v1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,27 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({ "email" => params["email"] })
+    if @user
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        session["user_id"] = @user["id"]
+        flash["notice"] = "Welcome, #{@user["first_name"]}."
+        redirect_to "/companies"
+      else
+        flash["notice"] = "Sorry, your password was incorrect."
+        redirect_to "/login"
+      end
+    else
+      flash["notice"] = "Sorry, we can't find that username."
+      redirect_to "/login"
+    end
   end
 
   def destroy
+    flash["notice"] = "Goodbye."
+    session["user_id"] = nil
+    redirect_to "/login"
+  end
   end
 end
   
